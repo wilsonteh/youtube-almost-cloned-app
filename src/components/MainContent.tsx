4bd6f798100a12f91a6c27c85de5dc3fa97f2547
Wilson <ywteh13@gmail.com>
2023-02-01 11:17:38 +0800

    Fetch trending videos on MainContent

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,23 +1,50 @@
+import { useEffect, useState } from "react";
 import VideoCard from "./VideoCard";
 
 const MainContent = () => {
+
+  const [videoList, setVideoList] = useState([]);
+
+  // useEffect(() => {
+  // }, [])
+    
+  const fetchVideos = () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '11f6926eedmshcb72d26a5c1ed22p145c4cjsn77082c63295d',
+        'X-RapidAPI-Host': 'yt-api.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://yt-api.p.rapidapi.com/trending?geo=MY', options)
+      .then(res => res.json())
+      .then(res => {
+        setVideoList(res.data)
+        console.log(res.data)
+      })
+      .catch(err => console.error(err));
+
+  }
+
+
   return (
-    <div className="p-6 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-x-[16px] gap-y-[48px]">
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-      <VideoCard />
-    </div>
+    <>
+      <button onClick={fetchVideos} className="bg-teal-500 px-4 py-1 rounded-2xl">
+        Fetch videos
+      </button>
+
+      <div className="p-6 grid grid-cols-1 xs:grid-cols-2 sm:grid-cols-3 xl:grid-cols-4 gap-x-[16px] gap-y-[48px]">
+
+        { videoList?.map(video => (
+          <VideoCard key={video.videoId} videoData={video}/>
+        ))}
+
+      </div>
+    
+    </>
   );
 }
+
  
 export default MainContent;
--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,36 +1,79 @@
+import { useEffect, useState } from "react";
 
-const VideoCard = () => {
+const VideoCard = ({ videoData }) => {
+
+  const [videoCardData, setVideoCardData] = useState({
+    videoId: "", 
+    videoTitle: "", 
+    thumbnailUrl: "", 
+    viewCount: 0, 
+    published: "", 
+    length: "",
+    channelId: "", 
+    channelTitle: "", 
+    channelPicUrl: "", 
+  })
+
+  let { videoId, title, channelId, channelTitle, viewCount, publishedText, lengthText } = videoData
+  let { url: thumbnailUrl } = videoData.thumbnail[2]
+  let { url: channelPicUrl } = videoData.channelThumbnail[0]
+  
+  useEffect(() => {
+    setVideoCardData(prev => ({ 
+      ...prev, 
+      videoId, 
+      videoTitle: title, 
+      thumbnailUrl, 
+      viewCount, 
+      published: publishedText,
+      length: lengthText,
+      channelId, 
+      channelTitle, 
+      channelPicUrl,
+    }))
+
+  }, [])
+  
+  
 
   return (
-    <div className="flex flex-col">
+    <a className="flex flex-col">
       <div className="relative">
-        <a href="">
-          <img className="rounded-xl" 
-            src="https://i.ytimg.com/vi/UbOaAtHWidc/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC_4pborog-GN879HOh7y8Pc2dwtw" alt="" />
+        <a href={`video/${videoCardData.videoId}`}>
+          <img className="rounded-xl" src={videoCardData.thumbnailUrl} 
+            alt={videoCardData.videoTitle} />
         </a>
-        <span className="bg-[hsla(0,0%,0%,0.8)] text-white font-medium text-xs px-1 rounded-sm absolute bottom-1 right-1">
-          34:50
+        <span className="bg-[hsla(0,0%,0%,0.8)] text-white font-medium text-xs px-1 
+          rounded-sm absolute bottom-1 right-1">
+          { videoCardData.length }
         </span>
       </div>
 
       <div className="pt-3 flex items-start">
-        <div className="w-[48px] h-[48px]">
-          <a href="">
-            <img className="rounded-full w-full" src="https://yt3.googleusercontent.com/ytc/AMLnZu80d66aj0mK3KEyMfpdGFyrVWdV5tfezE17IwRkhw=s176-c-k-c0x00ffffff-no-rj" alt="" />
+        <div className="w-[42px] h-[42px]">
+          <a href={`channel/${videoCardData.channelId}`}>
+            <img className="rounded-full w-full" src={videoCardData.channelPicUrl} 
+              alt={videoCardData.channelTitle} />
           </a>
         </div>
 
-        <div className="flex flex-col px-3">
-          <a href="" className="font-medium text-sm"> 
-            How to Fireship: Javascript Version Another Text??? 
+        <div className="flex flex-col px-3 w-full">
+          <a href={`video/${videoCardData.videoId}`} 
+            className="font-medium text-sm line-clamp-2"> 
+            {videoCardData.videoTitle}
           </a>
-          <a href="" className="mt-2 text-xs text-[hsl(0,0%,38%)]">Fireship</a>
-          <div className="text-xs text-[hsl(0,0%,38%)]">301K views • 2 days ago</div>
+          <a href={`channel/${videoCardData.channelId}`} 
+            className="mt-2 text-xs text-[hsl(0,0%,38%)] w-fit"> 
+            {videoCardData.channelTitle} 
+          </a>
+          <div className="text-xs text-[hsl(0,0%,38%)]">
+            {videoCardData.viewCount} views • {videoCardData.published}
+          </div>
         </div>
 
       </div>
 
-    </div>
+    </a>
   );
 }
  
